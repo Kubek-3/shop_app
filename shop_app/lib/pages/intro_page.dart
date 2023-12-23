ed603a170c96c6d0274e075e1e15ee09479206b8
Kubek-3 <jploskonka8@gmail.com>
2023-12-23 10:51:13 +0100

    initial commit

---
+++ shop_app/lib/pages/intro_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class IntroPage extends StatelessWidget {
+  const IntroPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: Center(
+        child: Column(
+          children: [
+            Icon(Icons.shopping_bag)
+          ],
+        ),
+      )
+    );
+  }
+}
---
+++ shop_app/lib/themes/light_theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+ThemeData lightMode = ThemeData(
+  colorScheme: ColorScheme.light(
+    background: Colors.blue.shade300,
+    primary: Colors.blue.shade100,
+    secondary:Colors.white,
+    inversePrimary: Colors.grey.shade600
+  ),
+);
